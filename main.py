e0fff9588df073b9675d727aeb2c802e9d10919f
JonneJBakker <j.j.bakker.15@student.rug.nl>
2025-10-10 12:20:02 +0200

    Split Data

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pandas as pd
+
+from utils.data_prep import split_data
 from utils.data_visualizer import visualize_data
 
 
 DATA_PATH = r'C:\Users\annad\PycharmProjects\Smell_Prediction\Data\Multi-Labelled_Smiles_Odors_dataset.csv'
 if __name__ == "__main__":
 
-    visualize_data(pd.read_csv(DATA_PATH))
+    data = pd.read_csv(DATA_PATH)
+    ## visualize_data(data)
+    split_data(data)
