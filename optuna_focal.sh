6547033f4e44ccec182e48dbfe6e8a8568328342
Jonne Julius Bakker J.J.Bakker.15@student.rug.nl <s3504026@login1.internal.hpc.rug.nl>
2025-12-16 13:14:21 +0100

    change :wq

--- optuna_focal.sh
+++ optuna_focal.sh
@@ -11,6 +11,6 @@ module load SciPy-bundle/2023.11-gfbf-2023b
 
 source $HOME/venvs/cBERTa/bin/activate
 
-python optuna_contrastive.py --train_csv Data/splits/train_stratified80.csv --val_csv Data/splits/val_stratified10.csv --smiles_column nonStereoSMILES --output_dir optuna_contrastive --n_trials 20
+python optuna_focal_loss.py --train_csv Data/splits/train_stratified80.csv --val_csv Data/splits/val_stratified10.csv --smiles_column nonStereoSMILES --output_dir optuna_asym_focal --n_trials 20
 
 deactivate
